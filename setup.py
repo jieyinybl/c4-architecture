8d18be4d1ad6e4cb93a736bc94232ffd7d054e84
Jieying Yan <jane.jieying.yan@gmail.com>
2020-11-23 21:08:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='c4-achitecture',
+    version='0.1dev',
+    packages=['graphviz',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
