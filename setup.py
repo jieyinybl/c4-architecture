e9fa4850a0ed595cfccc934de7283bb6e8715ce9
JieyingYan <jane.jieying.yan@gmail.com>
2020-11-23 21:49:14 +0100

    Update license, setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='c4-achitecture',
     version='0.1dev',
-    packages=['graphviz',],
+    install_requires=[
+        'graphviz'
+    ],
+    packages=find_packages(),
     license='MIT',
     long_description=open('README.md').read(),
+    url='https://github.com/jieyinybl/c4-architecture.git',
+    author='JieyingYan'
 )
